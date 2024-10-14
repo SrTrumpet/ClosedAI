43fc98a01ac1f3e6a3bfecff4e5c9b5acaaedd48
Elias M Olivares <elias.manque.o@gmail.com>
2024-10-13 23:24:26 -0300

    feat: Authentication entity functions added
        Added the login function, which returns a token
        and a message for the AuthResponse entity, also
        the AuthService constructor has userService, jwtService
        and authRepository.
    
    	modified:   Back/src/auth/auth.resolver.ts
    	modified:   Back/src/auth/auth.service.ts
    	new file:   Back/src/auth/entity/auth.entity.ts

--- Back/src/auth/auth.resolver.ts
+++ Back/src/auth/auth.resolver.ts
@@ -1,11 +1,25 @@
 import { Resolver, Args, Mutation, Query, Context } from "@nestjs/graphql";
 
+import { LoginDto } from "./dto/login.dto";
+import { ForgorPassDto } from "./dto/forgotpass.dto";
+import { AuthResponse } from "./entity/authresponse.entity";
+
+import { AuthService } from "./auth.service";
+
+
 @Resolver()
 export class AuthResolver {
+
+    constructor (private readonly authService : AuthService){}
 
     //Agregar mutation de registrar
     //Find User by Name
     //actualizar Informacion
     //Conseguir rol
+
+    @Mutation(returns => AuthResponse)
+    async login(@Args() logindDto : LoginDto ): Promise<AuthResponse>{
+        return this.authService.login(logindDto);
+    }
 
 }
--- Back/src/auth/auth.service.ts
+++ Back/src/auth/auth.service.ts
@@ -1,7 +1,52 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { jwtConstants } from "./constant/jwt.constants";
+
+import { JwtService } from "@nestjs/jwt";
+import { UserService } from "src/user/user.service";
+import { LoginDto } from "./dto/login.dto";
+
+import * as bcryptjs from "bcryptjs";
+import { InjectRepository } from "@nestjs/typeorm";
+import { AuthEntity } from "./entity/auth.entity";
+
+import { Repository } from "typeorm";
+import { AuthResponse } from "./entity/authresponse.entity";
 
 @Injectable()
 export class AuthService{
+
+    constructor(
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService,
+        @InjectRepository(AuthEntity)
+        private readonly authRepository : Repository<AuthEntity>,
+    ) {}
+
+    async login({ email, password }: LoginDto){
+        const user = await this.userService.findOneByEmail(email);
+
+        if(!user){
+            throw new UnauthorizedException("Email no valido");
+        }
+
+        const auth = await this.authRepository.findOne({where: {idUser: user.id}});
+        if (!auth) {
+            throw new UnauthorizedException("Usuario sin credenciales de autenticación");
+        }
+
+        const isPasswordValid = await bcryptjs.compare(password, auth.password);
+        if (!isPasswordValid) {
+            throw new UnauthorizedException("Contraseña o email no válido");
+        }
+
+        const payload = { email: user.email, id: user.id, name: user.firstName, lastName: user.lastName};
+        const token = await this.jwtService.signAsync(payload);
+
+        return {
+            message: "Inicio de sesion exitoso",
+            token
+        } as AuthResponse
+    }
 
     //Agregar service de register
     //conseguirInformacionUsaurio
