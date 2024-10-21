b0414d828069a6e94f0e142a9774a952efa2f6e0
Elias M Olivares <elias.manque.o@gmail.com>
2024-10-21 01:11:02 -0300

    feat: Graphql federation added
        Later, a gateway is added to control all
        microservices from a single point
    
    	modified:   Back/package-lock.json
    	modified:   Back/package.json
    	modified:   Back/src/app.module.ts

--- Back/src/app.module.ts
+++ Back/src/app.module.ts
@@ -1,32 +1,35 @@
 import { Module } from '@nestjs/common';
-
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-
 import { GraphQLModule } from '@nestjs/graphql';
-import {ApolloDriver, ApolloDriverConfig} from '@nestjs/apollo';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
 import { join } from 'path';
+import { GraphQLSchema } from 'graphql';
 
 @Module({
   imports: [
     UserModule,
     AuthModule,
     TypeOrmModule.forRoot({
-      type: "mysql",
-      host: "localhost",
+      type: 'mysql',
+      host: 'localhost',
       port: 3308,
-      database: "db_crud",
-      username: "crud_db",
-      password: "root",
+      database: 'db_crud',
+      username: 'crud_db',
+      password: 'root',
       autoLoadEntities: true,
-      synchronize: true, 
+      synchronize: true,
     }),
-
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src', 'graphql', 'schema.gql'),
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver:ApolloFederationDriver,
+      autoSchemaFile: {
+        path: join(process.cwd(), 'src', 'graphql', 'schema.gql'),
+        federation: 2,
+      },
       playground: true,
       introspection: true,
       context: ({ req }) => ({ req }),
